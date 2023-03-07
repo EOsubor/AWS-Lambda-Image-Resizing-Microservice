faf8e6101a9bddddf1f3eb48496b6623af615020
Ekene Osubor <46068348+EOsubor@users.noreply.github.com>
2023-03-07 19:41:24 +0000

    Refactoring
    The following improvements were made:
    
    - Use of the `with` statement to properly handle opening and closing of the image file.
    - Use of the `thumbnail()` method to maintain aspect ratio of the image while resizing.
    - Use of a buffer object (`BytesIO`) to avoid writing to disk during image processing.
    - Explicitly setting the output image format to JPEG for consistency.
    - Retrieving the desired width and height from the input event object and providing default values if not present.
    - Updating the output key to include the width and height information.
    - Returning the resized image information as a dictionary.

--- image_resize.py
+++ image_resize.py
@@ -1,29 +1,58 @@
 import boto3
-import os
-import sys
-import uuid
+from io import BytesIO
 from PIL import Image
-import PIL.Image
 
-s3 = boto3.client('s3')
 
-def resize_image(image_path, resized_path):
-    with Image.open(image_path) as image:
-        image.thumbnail(tuple(x / 2 for x in image.size))
-        image.save(resized_path)
+def resize_image(image_bytes, width, height):
+    """Resize an image to the specified dimensions.
+
+    Args:
+        image_bytes (bytes): Image bytes to resize.
+        width (int): Width of the output image.
+        height (int): Height of the output image.
+
+    Returns:
+        bytes: Resized image bytes.
+    """
+    with Image.open(BytesIO(image_bytes)) as img:
+        img.thumbnail((width, height))
+        buffer = BytesIO()
+        img.save(buffer, format='JPEG')
+        return buffer.getvalue()
+
 
 def lambda_handler(event, context):
-    for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        key = record['s3']['object']['key']
-        download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-        upload_path = '/tmp/resized-{}'.format(key)
-        
-        s3.download_file(bucket, key, download_path)
-        resize_image(download_path, upload_path)
-        s3.upload_file(upload_path, '{}-resized'.format(bucket), key)
+    """AWS Lambda function to resize images.
 
+    Args:
+        event (dict): AWS Lambda event object.
+        context (object): AWS Lambda context object.
+
+    Returns:
+        dict: Resized image data.
+    """
+    # Retrieve input data from the event object
+    s3_bucket = event['Records'][0]['s3']['bucket']['name']
+    s3_key = event['Records'][0]['s3']['object']['key']
+    width = event.get('width', 300)
+    height = event.get('height', 300)
+
+    # Download the image from S3
+    s3_client = boto3.client('s3')
+    image_object = s3_client.get_object(Bucket=s3_bucket, Key=s3_key)
+    image_bytes = image_object['Body'].read()
+
+    # Resize the image
+    resized_image = resize_image(image_bytes, width, height)
+
+    # Upload the resized image to S3
+    output_key = f"resized_{width}x{height}_{s3_key}"
+    s3_client.put_object(Bucket=s3_bucket, Key=output_key, Body=resized_image)
+
+    # Return the resized image information
     return {
-        'statusCode': 200,
-        'body': json.dumps('Image resizing complete')
+        'bucket': s3_bucket,
+        'key': output_key,
+        'width': width,
+        'height': height,
     }
